b7c742658ed07ecbdd40253adc428eb4324a8c51
Benjamin <Benjamin@BENJAMIN-PC>
2016-04-11 10:04:34 +0200

    Tuto Fini

--- indiarose_Todo/Resources/Resource.Designer.cs
+++ indiarose_Todo/Resources/Resource.Designer.cs
@@ -59,14 +59,52 @@ namespace indiarose_Todo
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Generated_ViewElement_0 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int Generated_ViewElement_1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int Generated_ViewElement_2 = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Home = 2130903040;
+			
+			static Layout()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Layout()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030001
-			public const int ApplicationName = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int ApplicationName = 2130968577;
 			
-			// aapt resource value: 0x7f030000
-			public const int Hello = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Hello = 2130968576;
 			
 			static String()
 			{
